915f4080859eebd9fb5aa4579c388de4617f54ed
Rafał Kochanecki <rafal.kochanecki@gmail.com>
2022-04-19 18:51:56 +0200

    task 1 done

--- 01/app.js
+++ 01/app.js
@@ -1,1 +1,6 @@
 console.log('DOM');
+
+const element = document.querySelector('.comments__item, comments__item--newest')
+const attributeCount = element.querySelectorAll('span')
+
+console.log(attributeCount.length)
