095d011836a9f6ba50f8f2e8794e9dea79d6b767
Rafał Kochanecki <rafal.kochanecki@gmail.com>
2022-04-20 18:10:45 +0200

    task 2 done

--- 02/app.js
+++ 02/app.js
@@ -1,1 +1,11 @@
 console.log('DOM');
+
+const ul = document.querySelectorAll('a')
+let attribute
+
+for (let i = 0; i < ul.length; i++) {
+    if (ul[i].getAttribute('data-url')) {
+        attribute = ul[i].getAttribute('data-url')
+        ul[i].setAttribute('href', attribute)
+    }
+}
