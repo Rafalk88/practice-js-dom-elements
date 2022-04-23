ca0e40e7da851f6df01a3eb44f033205cccce917
Rafał Kochanecki <rafal.kochanecki@gmail.com>
2022-04-23 15:11:36 +0200

    without structure created

--- 04/app.js
+++ 04/app.js
@@ -6,3 +6,45 @@ const menuItems = [
     {text: 'galeria', url: '/gallery'},
     {text: 'kontakt', url: '/contact'},
 ];
+
+const liItems = 3 // number of li items in ul
+
+// ------------ Without structure above -------------------
+
+// ----------------- Creating ul --------------------------
+const navSection = document.querySelector('nav')
+const ul = document.createElement('ul')
+navSection.insertBefore(ul, null)
+// --------------------------------------------------------
+
+// ------------ Creating three li in ul -------------------
+const ulSection = document.querySelector('ul')
+for (let i = 0; i < liItems; i++) {
+
+    const li = document.createElement('li')
+    ulSection.insertBefore(li, null)
+
+}
+// --------------------------------------------------------
+
+// ------------ Create a for li items ---------------------
+const liSection = document.querySelectorAll('li')
+for (let i = 0; i < liItems; i++) {
+
+    const a = document.createElement('a')
+    liSection[i].insertBefore(a, null)
+
+}
+// --------------------------------------------------------
+
+// ------ Create attributes for a in li elements ----------
+const aSection = document.querySelectorAll('a')
+aSection[0].innerText = 'home'
+aSection[0].setAttribute('href', '/')
+
+aSection[1].innerText = 'gallery'
+aSection[1].setAttribute('href', '/gallery')
+
+aSection[2].innerText = 'contact'
+aSection[2].setAttribute('href', '/contact')
+// --------------------------------------------------------
